5ae20fce2b20225b9f4371bcb23c736bcbab8713
Nethelbert <gilblasse@gmail.com>
2020-02-28 05:11:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+let batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 function totalBatteries(){
-  batteryBatches.reduce(sum,num => num + sum);
+  return batteryBatches.reduce(sum,num => num + sum);
 }
